170bca1c7939355e78fd4a06c3e1281d5860a705
Joaquim Monserrat <joaquim.monserrat@techedgegroup.com>
2022-04-26 08:41:02 +0200

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,31 @@ import { GymsComponent } from './gyms/gyms.component';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { BoulderListComponent } from './boulders/boulder-list/boulder-list.component';
 import { BoulderDetailComponent } from './boulders/boulder-detail/boulder-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, GymsComponent, BoulderListComponent, BoulderDetailComponent],
+  declarations: [
+    AppComponent,
+    GymsComponent,
+    BoulderListComponent,
+    BoulderDetailComponent,
+    HeaderComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatListModule,
     MatDividerModule,
+    MatToolbarModule,
     MatIconModule,
-    HttpClientModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
